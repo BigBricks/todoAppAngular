34615a90186c45cdcc9819ee3f9e5c66d41f320a
unknown <bsa6.23.94@gmail.com>
2018-09-28 15:22:07 -0400

    updated todo tests

--- todo-app/src/app/todo.spec.ts
+++ todo-app/src/app/todo.spec.ts
@@ -4,4 +4,12 @@ describe("Todo", () => {
   it("should create an instance", () => {
     expect(new Todo()).toBeTruthy();
   });
+  it("should accept values into constructor", () => {
+    let bob = new Todo({
+      title: "WHAT ARE YOU DOING PRIVATE",
+      complete: true
+    });
+    expect(bob.title).toEqual("WHAT ARE YOU DOING PRIVATE");
+    expect(bob.complete).toEqual(true);
+  });
 });
