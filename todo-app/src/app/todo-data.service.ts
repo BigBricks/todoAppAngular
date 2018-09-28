5734b38969d8f00edf42591b311af62342dadcad
unknown <bsa6.23.94@gmail.com>
2018-09-28 17:37:39 -0400

    todo data service update and get by id todo

--- todo-app/src/app/todo-data.service.ts
+++ todo-app/src/app/todo-data.service.ts
@@ -18,4 +18,15 @@ export class TodoDataService {
     this.todos = this.todos.filter(todo => todo.id !== id);
     return this;
   }
+  updateTodo(id: number, values: Object = {}): Todo {
+    let bob = this.getTodo(id);
+    if (!bob) {
+      return null;
+    }
+    Object.assign(bob, values);
+    return bob;
+  }
+  getTodo(id: number): Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
+  }
 }
