10d419ff4a92d0089b2d2b36cf1c49ef526c418c
unknown <bsa6.23.94@gmail.com>
2018-10-01 17:19:19 -0400

    work

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from "@angular/core";
+import { Todo } from "./todo";
 import { TodoDataService } from "./todo-data.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  providers: [TodoDataService]
 })
 export class AppComponent {
   title = "todo-app";
+  newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {}
   toggleComplete(todo) {
     this.todoDataService.toggleComplete(todo);
+  }
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodo(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodo();
   }
 }
