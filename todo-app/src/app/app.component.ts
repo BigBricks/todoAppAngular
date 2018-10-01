828a2f0a5edbb3974fe6abb7c741fa4925065284
unknown <bsa6.23.94@gmail.com>
2018-10-01 17:16:02 -0400

    working on the app

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TodoDataService } from "./todo-data.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = "todo-app";
+  constructor(private todoDataService: TodoDataService) {}
+  toggleComplete(todo) {
+    this.todoDataService.toggleComplete(todo);
+  }
 }
