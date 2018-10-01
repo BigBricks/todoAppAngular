8c01016b7233b89fee9fe614ae63ee7eb8d5f4f0
unknown <bsa6.23.94@gmail.com>
2018-10-01 11:24:30 -0400

    working on the tests for data service

--- todo-app/src/app/todo-data.service.spec.ts
+++ todo-app/src/app/todo-data.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from "@angular/core/testing";
-
+import { Todo } from "./todo";
 import { TodoDataService } from "./todo-data.service";
 
 describe("TodoDataService", () => {
@@ -11,5 +11,14 @@ describe("TodoDataService", () => {
   it("should be created", () => {
     const service: TodoDataService = TestBed.get(TodoDataService);
     expect(service).toBeTruthy();
+  });
+  describe("#getAllTodo()", () => {
+    it(
+      "should return an empty array by default",
+      inject([TodoDataService]),
+      (service: TodoDataService) => {
+        expect(service.getAllTodo()).toEqual([]);
+      }
+    );
   });
 });
