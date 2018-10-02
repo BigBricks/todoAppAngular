0e92e569ee08fe51c27da22263a2c20c976e4a2e
unknown <bsa6.23.94@gmail.com>
2018-10-02 14:55:03 -0400

    added some base code and updated imports

--- todo-app/src/app/api.service.ts
+++ todo-app/src/app/api.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../environments/environment";
-import { Http } from "@angular/http";
-
+import { Http, Response } from "@angular/http";
+import { Todo } from "./todo";
+import { Observable } from "rxjs/Observable";
 const API_URL = environment.apiUrl;
 @Injectable({
   providedIn: "root"
 })
 export class APIService {
-  constructor() {}
+  constructor(private http: Http) {}
+  public addTodo(todo: Todo) {}
+  public deleteTodo() {}
+  public updateTodo() {}
+  public getTodo() {}
+  public getAllTodo() {}
+  public toggleComplete() {}
 }
