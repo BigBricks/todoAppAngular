454259debb12d0658297d7ad2ba5e45fb298b057
unknown <bsa6.23.94@gmail.com>
2018-10-02 14:51:43 -0400

    added imports to the api service

--- todo-app/src/app/api.service.ts
+++ todo-app/src/app/api.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment";
+import { Http } from "@angular/http";
 
+const API_URL = environment.apiUrl;
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class APIService {
-
-  constructor() { }
+  constructor() {}
 }
