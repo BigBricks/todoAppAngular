c9da0a891f48220e5ca93eb719b6f78e6f04dfa4
unknown <bsa6.23.94@gmail.com>
2018-10-02 17:43:34 -0400

    updated backend

--- backend/todo/todo.js
+++ backend/todo/todo.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Todo = mongoose.Schema({
-  todo: { type: String, required: true }
+  id: { type: Number, required: true },
+  title: { type: String, required: true },
+  complete: { type: Boolean, required: true }
 });
 
 module.exports = mongoose.model("Todo", Todo);
